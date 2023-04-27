3b4b1ed16c03c2dd37e1470f1e854ca09cc25ce6
Max! <admin@cloudydev.net>
2023-04-27 10:31:09 +0200

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.47.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "4.47.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+provider "google" {
+  project     = "gpu-cloud-init-project"
+  region      = "europe-west4"
+  credentials = "/terraform/terraform-key"
+}
+
+provider "random" {
+}
+
+provider "google-beta" {
+}

04342a20901e98b11a3486ab868ddddfa9360931
Max! <admin@cloudydev.net>
2023-05-22 20:45:56 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.47.0"
+      version = ">=4.47.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "4.47.0"
+      version = ">=4.47.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.4.3"
+      version = ">=3.4.3"
     }
   }
 }
